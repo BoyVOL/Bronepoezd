abc8a31aa1b3810e5335fd633d4461db2b19b03d
BoyVOL <vladislav_limonov@mail.ru>
2025-08-27 14:38:01 +0900

    Создан модуль

---
+++ bronepoezd/Scene2.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public partial class Scene2 : Node2D
+{
+    [Export] Node2D Test;
+
+    public override void _EnterTree()
+    {
+        base._EnterTree();
+    }
+
+}
