7815ada92ed7479861e636c662a90d9de9e91936
BoyVOL <vladislav_limonov@mail.ru>
2025-09-04 23:49:17 +0900

    Реализован и протестирован модуль

---
+++ bronepoezd/addons/ParentSwitchModule/ParentSwitcherNode2D/ParentSwitcherNode2D.cs
@@ -0,0 +1,43 @@
+using Godot;
+using Godot.Collections;
+using System;
+
+public partial class ParentSwitcherNode2D : Node2D
+{
+	[Export] public ParentSwitcherNode2D OldParent = null;
+
+	[Export] Array<Array<NodePath>> Links = new Array<Array<NodePath>>();
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	public void RebaseNodes()
+	{
+		foreach (Array<NodePath> Link in Links)
+		{
+			for (int i = 1; i < Link.Count; i++)
+			{
+				Node Test = GetNode<Node>(Link[i]);
+				Test.Reparent(GetNode<Node>(Link[0]));
+				if (Test is ParentSwitcherNode2D)
+				{
+					((ParentSwitcherNode2D)Test).OldParent = this;
+				}
+			}
+		}
+	}
+
+	public override void _EnterTree()
+	{
+		base._EnterTree();
+		OldParent = GetParentOrNull<ParentSwitcherNode2D>();
+		RebaseNodes();
+	}
+
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
