953b4a0cb6800de2350c7a6420a20b920afe863c
BoyVOL <vladislav_limonov@mail.ru>
2025-09-17 18:26:13 +0900

    Добавлен метод по свапу на след рельсу

--- bronepoezd/addons/TrainPhysicsModule/Nodes/SingleRail/SingleRail.cs
+++ bronepoezd/addons/TrainPhysicsModule/Nodes/SingleRail/SingleRail.cs
@@ -9,6 +9,38 @@ public partial class SingleRail : Rail, IRail
     [Export] public Rail PrevRail = null;
     [Export] public bool PrevRailReverse = false;
 
+    public void SwapToRail(Train train, IRail rail)
+    {
+        if (train.railPos > Curve.GetBakedLength() / 2)
+        {
+            if (rail.Curve.GetClosestOffset(Curve.SampleBaked(Curve.GetBakedLength())) > rail.Curve.GetBakedLength() / 2)
+            {
+                //Head of old rail, head of new rail
+                train.railPos = rail.Curve.GetBakedLength();
+                train.reverse = !train.reverse;
+            }
+            else
+            {
+                //HEad of old rail, tail of new rail                
+                train.railPos = 0;
+            }
+        }
+        else
+        {
+            if (rail.Curve.GetClosestOffset(Curve.SampleBaked(Curve.GetBakedLength())) > rail.Curve.GetBakedLength() / 2)
+            {
+                //Tail of old rail, head of new rail
+                train.railPos = rail.Curve.GetBakedLength();
+            }
+            else
+            {
+                //Tail of old rail, tail of new rail
+                train.railPos = 0;
+                train.reverse = !train.reverse;
+            }
+        }
+    }
+
     void IRail.MoveToNext(Train train)
     {
         if (NextRail != null)
