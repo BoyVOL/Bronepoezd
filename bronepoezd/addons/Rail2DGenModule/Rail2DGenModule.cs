bedfe6e59d89b1be8c0f504c749a30016cbca61c
BoyVOL <vladislav_limonov@mail.ru>
2025-09-22 10:01:44 +0900

    Создана и прописана нода

--- bronepoezd/addons/Rail2DGenModule/Rail2DGenModule.cs
+++ bronepoezd/addons/Rail2DGenModule/Rail2DGenModule.cs
@@ -5,14 +5,21 @@ using System;
 [Tool]
 public partial class Rail2DGenModule : EditorPlugin
 {
+    String GlobalPath;
 	public override void _EnterTree()
 	{
+		GlobalPath = ((Resource)GetScript()).ResourcePath.GetBaseDir();
+        AddCustomType("RailGenerator", "Path2D", GD.Load<Script>(GlobalPath + "/Nodes/RailGenerator/RailGenerator.cs"),
+        GD.Load<Texture2D>(GlobalPath + "/Nodes/RailGenerator/icon.png"));
 		// Initialization of the plugin goes here.
+        GD.Print("Rail2DGenModule loaded");
 	}
 
 	public override void _ExitTree()
 	{
+		RemoveCustomType("RailGenerator");
 		// Clean-up of the plugin goes here.
+		GD.Print("Rail2DGenModule unloaded");
 	}
 }
 #endif
