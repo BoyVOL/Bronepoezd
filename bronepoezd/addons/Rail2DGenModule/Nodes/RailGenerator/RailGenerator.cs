8aa40f749cee83e6d9758ab5e1e5c414db366a04
BoyVOL <vladislav_limonov@mail.ru>
2025-09-22 11:15:44 +0900

    Сделал границы отображаемыми в дебажной версии

--- bronepoezd/addons/Rail2DGenModule/Nodes/RailGenerator/RailGenerator.cs
+++ bronepoezd/addons/Rail2DGenModule/Nodes/RailGenerator/RailGenerator.cs
@@ -1,15 +1,48 @@
 using Godot;
 using System;
+using System.Diagnostics;
 
 [Tool]
 [GlobalClass]
-public partial class RailGenerator : Node
+public partial class RailGenerator : Node2D
 {
-    [ExportToolButton("Generate")]
-    public Callable GenerateButton => Callable.From(Generate);
+	public Node2D Parent;
 
-    public void Generate()
-    {
-        
-    }
+	[Export]
+	Vector2 GenBoundaries = new Vector2(0, 0);
+
+	[Export]
+	Color DebugBorderColor = Colors.Aqua;
+
+	[ExportToolButton("Generate")]
+	public Callable GenerateButton => Callable.From(Generate);
+
+	public void Generate()
+	{
+
+	}
+
+	public override void _EnterTree()
+	{
+		base._EnterTree();
+		Parent = GetParent<Node2D>();
+	}
+
+
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
+		QueueRedraw();
+	}
+
+	public override void _Draw()
+	{
+		base._Draw();
+		#if DEBUG
+		{
+			DrawRect(new Rect2(Vector2.Zero, GenBoundaries), DebugBorderColor, false, 5);
+		}
+		#endif
+	}
+
 }
