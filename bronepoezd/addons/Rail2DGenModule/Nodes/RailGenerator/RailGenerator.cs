f164db43c06121c0d66116d6455087070cc29a14
BoyVOL <vladislav_limonov@mail.ru>
2025-09-22 16:06:37 +0900

    Добавлен метод генерации точек.

--- bronepoezd/addons/Rail2DGenModule/Nodes/RailGenerator/RailGenerator.cs
+++ bronepoezd/addons/Rail2DGenModule/Nodes/RailGenerator/RailGenerator.cs
@@ -14,12 +14,73 @@ public partial class RailGenerator : Node2D
 	[Export]
 	Color DebugBorderColor = Colors.Aqua;
 
+	[Export]
+	int JunctCount = 1;
+
+	[Export]
+	double MinGenDistance = 100;
+
+	[Export]
+	PackedScene StraitRail;
+
+	[Export]
+	PackedScene Junction;
+
 	[ExportToolButton("Generate")]
 	public Callable GenerateButton => Callable.From(Generate);
 
 	public void Generate()
 	{
+		RemoveAllChildren();
+		Vector2[] JunctPoints = GenJunctionPoints(MinGenDistance);
+		foreach (var item in JunctPoints)
+		{
+			GenerateJunction(item);
+		}
+	}
 
+	public void RemoveAllChildren()
+	{
+		foreach (Node child in GetChildren())
+		{
+			RemoveChild(child);
+			child.QueueFree();
+		} 
+	}
+
+	public Vector2[] GenJunctionPoints(double minDistance = 0)
+	{
+		Vector2[] Result = new Vector2[JunctCount];
+		for (int i = 0; i < Result.Length; i++)
+		{
+			int retries = 0;
+			bool FarEnough = false;
+			do
+			{
+				FarEnough = true;
+				Result[i] = new Vector2(GD.Randf() * GenBoundaries.X, GD.Randf() * GenBoundaries.Y);
+				for (int j = 0; j < i; j++)
+				{
+					if ((Result[i] - Result[j]).LengthSquared() < minDistance * minDistance)
+					{
+						FarEnough = false;
+						break;
+					}
+				}
+			} while (!FarEnough);
+		}
+		return Result;
+	}
+
+	public void GenerateJunction(Vector2 Pos)
+	{
+		if (Junction != null)
+		{
+			Node2D Buff = Junction.Instantiate<Node2D>(PackedScene.GenEditState.Instance);
+			AddChild(Buff);
+			Buff.Position = Pos;
+			Buff.QueueRedraw();
+		}
 	}
 
 	public override void _EnterTree()
