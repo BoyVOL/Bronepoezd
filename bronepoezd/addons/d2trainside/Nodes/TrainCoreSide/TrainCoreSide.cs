811e3ac8a3870fba0e73e8a5b21680c7596715f3
BoyVOL <vladislav_limonov@mail.ru>
2025-10-02 16:45:23 +0900

    изменения в сцене

--- bronepoezd/addons/d2trainside/Nodes/TrainCoreSide/TrainCoreSide.cs
+++ bronepoezd/addons/d2trainside/Nodes/TrainCoreSide/TrainCoreSide.cs
@@ -2,6 +2,7 @@ using Godot;
 using System;
 
 [GlobalClass]
-public partial class TrainCoreSide : ParentSwitcherNode2D
+public partial class TrainCoreSide : Node2D
 {
+    [Export] Train TopDownOrigin = null;
 }
