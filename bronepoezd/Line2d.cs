f3719ddcb6f5ef779464027c26b47955f2dcf151
BoyVOL <vladislav_limonov@mail.ru>
2025-10-04 21:53:04 +0900

    Добавлен шейдер с матрицей трансформации

--- bronepoezd/Line2d.cs
+++ bronepoezd/Line2d.cs
@@ -2,32 +2,29 @@ using Godot;
 using System;
 using System.Drawing;
 
-[Tool]
 public partial class Line2d : Line2D
 {
     [Export]
-    Curve2D Curve = null;
+    Train train = null;
 
     public override void _Draw()
     {
         base._Draw();
-        if (Curve != null)
-        {
-            Points = Curve.GetBakedPoints();
-        }
     }
 
     public override void _PhysicsProcess(double delta)
     {
         base._PhysicsProcess(delta);
-        Curve.SetPointPosition(0, Curve.GetPointPosition(0) + Vector2.Right * 10);
-        GD.Print("Test");
     }
 
     public override void _Process(double delta)
     {
         base._Process(delta);
-        GD.Print("Test2");
+        if (train != null)
+        {
+		    ShaderMaterial ShadMat = Material as ShaderMaterial;
+		    ShadMat.SetShaderParameter("speed", (train.RailSpeed/10)*-1);
+        }
     }
 
 }
