b1b5d80d0845677247501e08a01e38252df25d34
BoyVOL <vladislav_limonov@mail.ru>
2025-10-07 17:30:10 +0900

    Добавлен масштаб

--- bronepoezd/addons/autoscaling_viewport_module/AutoscalingViewport/AutoscalingViewport.cs
+++ bronepoezd/addons/autoscaling_viewport_module/AutoscalingViewport/AutoscalingViewport.cs
@@ -7,6 +7,9 @@ public partial class AutoscalingViewport : SubViewport
     [Export]
     Node ScalingSource = null;
 
+    [Export]
+    Vector2 Scale = Vector2.One;
+
     public override void _EnterTree()
     {
         base._EnterTree();
@@ -16,6 +19,6 @@ public partial class AutoscalingViewport : SubViewport
     public override void _Process(double delta)
     {
         base._Process(delta);
-        if(ScalingSource != null) Size = (Vector2I)ScalingSource.GetViewport().GetVisibleRect().Size;
+        if(ScalingSource != null) Size = (Vector2I)(ScalingSource.GetViewport().GetVisibleRect().Size*Scale);
     }
 }
