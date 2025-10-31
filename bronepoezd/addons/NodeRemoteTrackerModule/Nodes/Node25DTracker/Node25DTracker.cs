4db54386544d0250b435e9c4edf92b36a046e97b
BoyVOL <vladislav_limonov@mail.ru>
2025-10-31 14:15:30 +0900

    Добавил эммиттер частиц и исправил инверсию угла поворота

--- bronepoezd/addons/NodeRemoteTrackerModule/Nodes/Node25DTracker/Node25DTracker.cs
+++ bronepoezd/addons/NodeRemoteTrackerModule/Nodes/Node25DTracker/Node25DTracker.cs
@@ -28,19 +28,19 @@ public partial class Node25DTracker : Node3D
             case Planes.XY:
                 {
                     Position = new Vector3(Source.Position.X*ProjectionScale.X, Source.Position.Y*ProjectionScale.Y, 0);
-                    Rotation = new Vector3(0, 0, Source.Rotation);
+                    Rotation = new Vector3(0, 0, -Source.Rotation);
                     break;
                 }
             case Planes.XZ:
                 {
                     Position = new Vector3(Source.Position.X*ProjectionScale.X, 0, Source.Position.Y*ProjectionScale.Y);
-                    Rotation = new Vector3(0, Source.Rotation, 0);
+                    Rotation = new Vector3(0, -Source.Rotation, 0);
                     break;
                 }
             case Planes.YZ:
                 {
                     Position = new Vector3(0, Source.Position.X*ProjectionScale.X, Source.Position.Y*ProjectionScale.Y);
-                    Rotation = new Vector3(Source.Rotation, 0, 0);
+                    Rotation = new Vector3(-Source.Rotation, 0, 0);
                     break;
                 }
             default: throw new ArgumentException("Unexplained plane case");
