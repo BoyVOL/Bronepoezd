15713572b5d264eb2ae378de3425c695e3089ef3
BoyVOL <vladislav_limonov@mail.ru>
2025-10-31 13:17:19 +0900

    Дооформил варианты.

--- bronepoezd/addons/NodeRemoteTrackerModule/Nodes/Node25DTracker/Node25DTracker.cs
+++ bronepoezd/addons/NodeRemoteTrackerModule/Nodes/Node25DTracker/Node25DTracker.cs
@@ -9,7 +9,7 @@ public partial class Node25DTracker : Node3D
     {
         XY,
         XZ,
-        ZY,
+        YZ,
     }
 
     [Export] public Node2D Source = null;
@@ -28,15 +28,19 @@ public partial class Node25DTracker : Node3D
             case Planes.XY:
                 {
                     Position = new Vector3(Source.Position.X*ProjectionScale.X, Source.Position.Y*ProjectionScale.Y, 0);
-                    Rotation = new Vector3(0, Source.Rotation, 0);
+                    Rotation = new Vector3(0, 0, Source.Rotation);
                     break;
                 }
             case Planes.XZ:
                 {
+                    Position = new Vector3(Source.Position.X*ProjectionScale.X, 0, Source.Position.Y*ProjectionScale.Y);
+                    Rotation = new Vector3(0, 0, Source.Rotation);
                     break;
                 }
-            case Planes.ZY:
+            case Planes.YZ:
                 {
+                    Position = new Vector3(0, Source.Position.X*ProjectionScale.X, Source.Position.Y*ProjectionScale.Y);
+                    Rotation = new Vector3(Source.Rotation, 0, 0);
                     break;
                 }
             default: throw new ArgumentException("Unexplained plane case");
