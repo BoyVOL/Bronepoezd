87b932fa1e09d20a607bc70c3213af5a39a85685
BoyVOL <vladislav_limonov@mail.ru>
2026-07-01 17:29:36 +0900

    Слил две ноды в одну

--- bronepoezd/Scenes/Test Scenes/RailDirector.cs
+++ bronepoezd/Scenes/Test Scenes/RailDirector.cs
@@ -1,31 +1,24 @@
 using Godot;
 using System;
 
-public partial class RailDirector : Node
+public partial class RailDirector : MovePid
 {
-    [Export] Node PID = null;
     [Export] Path3D Rail = null;
-
-    MovePid pid = null;
     [Export] float factor = 0f;
-    [Export] float Speed = 0f;
 
     public override void _EnterTree()
     {
         base._EnterTree();
-        if(PID != null) pid = (MovePid)PID;
     }
 
     public override void _PhysicsProcess(double delta)
     {
-        base._PhysicsProcess(delta);
-        if(pid != null && Rail != null)
+        if(Rail != null)
         {
-            Vector3 TargetVector = (Rail.Curve.GetClosestPoint(pid.ControlledNode.Position) - pid.ControlledNode.Position)*factor;
-            float SpeedSampleOffset = Rail.Curve.GetClosestOffset(pid.ControlledNode.Position);
-            Vector3 SpeedVector = Rail.Curve.SampleBaked(SpeedSampleOffset+0.1f)-Rail.Curve.SampleBaked(SpeedSampleOffset);
-            SpeedVector = SpeedVector.Normalized()*Speed;
-            pid.DesiredMove = TargetVector+SpeedVector;
+            Vector3 TargetVector = (Rail.Curve.GetClosestPoint(ControlledNode.Position) - ControlledNode.Position)*factor;
+            float SpeedSampleOffset = Rail.Curve.GetClosestOffset(ControlledNode.Position);
+            DesiredMove = TargetVector;
         }
+        base._PhysicsProcess(delta);
     }
 }
